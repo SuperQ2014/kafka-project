a31bd690f90dfe522c7614c018772048eb32e338
chaoqiang <tao-chqang@163.com>
2016-04-01 21:28:49 +0800

    .

--- simple-producer/src/main/java/skyler/tao/kafka/SimpleProducer.java
+++ simple-producer/src/main/java/skyler/tao/kafka/SimpleProducer.java
@@ -14,14 +14,14 @@ public class SimpleProducer {
 		Properties prop = new Properties();
 //		prop.put("metadata.broker.list", "172.16.89.128:9092");
 		prop.put("bootstrap.servers", "172.16.89.128:9092,172.16.89.129:9092,172.16.89.130:9092");
-		prop.put("request.required.acks", "1");		//unknown config
-		prop.put("serializer.class", StringSerializer.class.getName());		//unknown config
+		prop.put("acks", "all");		//not request.required.acks
+//		prop.put("serializer.class", StringSerializer.class.getName());		//unknown config
 		prop.put("key.serializer", StringSerializer.class.getName());
 		prop.put("value.serializer", StringSerializer.class.getName());
 		
 		KafkaProducer<Integer, String> producer = new KafkaProducer<Integer, String>(prop);
 		
-		for (int iCount = 0; iCount < 1000; iCount++) {
+		for (int iCount = 0; iCount < 10; iCount++) {
 			String message = "My Test Message No " + iCount;
 			ProducerRecord<Integer, String> record = new ProducerRecord<Integer, String>("test-topic-tao", message);
 			producer.send(record);
